b158a9fd068d2592604b7cb09ef090a6bdbf1db2
carloseam94 <carloseam94@gmail.com>
2025-06-11 16:34:49 +0200

    fix: recommendationAnswer file name instead of cover

--- models/recommendationAnswer.go
+++ models/recommendationAnswer.go
@@ -3,7 +3,7 @@ package models
 type RecommendationAnswer struct {
 	ID               uint           `json:"id" gorm:"primaryKey;autoIncrement"`
 	Form             string         `json:"form"`
-	File             string         `json:"cover" gorm:"type:varchar(500)"` // Path or URL to file
+	File             string         `json:"file" gorm:"type:varchar(500)"` // Path or URL to file
 	RecommendationID uint           `json:"recommendationId"`
 	Recommendation   Recommendation `gorm:"foreignKey:RecommendationID"`
 }
