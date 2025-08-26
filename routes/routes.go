bb43fd8f71ab7efcaf0f8f2fa6b9cc130b3b5745
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2025-08-26 15:21:21 +0200

    Allow all ports on localhost

--- routes/routes.go
+++ routes/routes.go
@@ -12,7 +12,7 @@ func SetupRouter() *gin.Engine {
 
 	// CORS middleware configuration
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},                   // Take from env variable
+		AllowOrigins:     []string{"http://localhost", "http://localhost:*"},                   // Take from env variable
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},            // Allow HTTP methods
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Allow headers
 		AllowCredentials: true,                                                // Allow cookies if needed
