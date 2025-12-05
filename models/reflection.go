584dd0d753830e6444f337411e6b640a1b8b8eef
Carlos <carloseam94@gmail.com>
2025-12-05 11:13:00 +0100

    feat: chnge models to describe new format

--- models/reflection.go
+++ models/reflection.go
@@ -1,9 +1,10 @@
 package models
 
 type Reflection struct {
-	ID          uint   `json:"id" gorm:"primaryKey;autoIncrement"`
-	Description string `json:"description"`
-	Form        string `json:"form"`
-	PhaseID     uint   `json:"phaseId"`
-	Phase       Phase  `gorm:"foreignKey:PhaseID"` // Relationship to the phase
+	ID             uint   `json:"id" gorm:"primaryKey;autoIncrement"`
+	Title          string `json:"title" gorm:"type:varchar(255);not null"`
+	Description    string `json:"description"`
+	Considerations string `json:"considerations" gorm:"type:text"`
+	PhaseID        uint   `json:"phaseId"`
+	Phase          Phase  `gorm:"foreignKey:PhaseID"` // Relationship to the phase
 }
--- models/reflectionAnswer.go
+++ models/reflectionAnswer.go
@@ -3,11 +3,11 @@ package models
 import "github.com/google/uuid"
 
 type ReflectionAnswer struct {
-	ID               uint       `json:"id" gorm:"primaryKey;autoIncrement"`
-	Form             string     `json:"form"`
-	BinaryEvaluation uint       `json:"binaryEvaluation"`
-	ReflectionID     uint       `json:"reflectionId"`
-	Reflection       Reflection `gorm:"foreignKey:ReflectionID"`
-	UserID           uuid.UUID  `json:"userId"`
-	User             User       `gorm:"foreignKey:UserID"`
+	ID                 uint       `json:"id" gorm:"primaryKey;autoIncrement"`
+	ReflectionID       uint       `json:"reflectionId"`
+	Reflection         Reflection `gorm:"foreignKey:ReflectionID"`
+	UserID             uuid.UUID  `json:"userId"`
+	User               User       `gorm:"foreignKey:UserID"`
+	AnswerText         string     `json:"answerText" gorm:"type:text"`
+	GetRecommendations bool       `json:"getRecommendations"`
 }
