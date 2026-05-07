089de5270591579ac72ca7a5a5e41d01af7c7485
Milan Dirks <159008288+Cosmogos@users.noreply.github.com>
2026-05-07 09:44:59 +0200

    feat: add UpdatedAt field to FurtherReflectionAnswer and RecommendationAnswer models

--- models/recommendationAnswer.go
+++ models/recommendationAnswer.go
@@ -1,16 +1,18 @@
 package models
 
+import "time"
 import (
 	"github.com/google/uuid"
 )
 
 type RecommendationAnswer struct {
-	ID               uint           `json:"id" gorm:"primaryKey;autoIncrement"`
-	Form             string         `json:"form"`
-	File             string         `json:"file" gorm:"type:varchar(500)"` // Path or URL to file
-	CheckedDone      bool           `json:"checked_done" gorm:"default:false"`
-	RecommendationID uint           `json:"recommendationId"`
-	Recommendation   Recommendation `gorm:"foreignKey:RecommendationID"`
-	UserID           uuid.UUID      `json:"userId"`
-	User             User           `gorm:"foreignKey:UserID"`
+ID               uint           `json:"id" gorm:"primaryKey;autoIncrement"`
+Form             string         `json:"form"`
+File             string         `json:"file" gorm:"type:varchar(500)"` // Path or URL to file
+CheckedDone      bool           `json:"checked_done" gorm:"default:false"`
+RecommendationID uint           `json:"recommendationId"`
+Recommendation   Recommendation `gorm:"foreignKey:RecommendationID"`
+UserID           uuid.UUID      `json:"userId"`
+User             User           `gorm:"foreignKey:UserID"`
+UpdatedAt        time.Time      `json:"updatedAt"`
 }
